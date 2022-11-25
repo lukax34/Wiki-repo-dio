716070365c660da3ebe2cf81f413515407b7d691
Lucas Feliphe Aleixo <lukax34@outlook.com>
2022-11-25 14:01:19 -0300

    Adicionado botão remover

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,8 @@ function App() {
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
+    const repoRemove= repos.filter((repo) => repo.id !== id);
+    setRepos(repoRemove);
   }
 
 
